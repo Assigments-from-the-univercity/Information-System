c329f56936e56623f545ddba01c3a1d2b6073636
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-01-27 13:14:51 +0200

    start

---
+++ cmake-build-debug/CMakeFiles/Information_System.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Information_System.dir/main.cpp.o"
+  "Information_System"
+  "Information_System.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Information_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Information_System.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Information_System.
+# This may be replaced when dependencies are built.
